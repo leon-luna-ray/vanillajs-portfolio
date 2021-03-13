141558dcd3c74cb1ed37b6fca3e7892f7cdc5c97
Ray Luna <leon.luna.ray@gmail.com>
2021-03-12 20:35:09 -0800

    add featured project

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Materialize Image Code
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
