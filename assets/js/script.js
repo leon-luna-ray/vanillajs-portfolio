edc4a857b2f56935943037fc2cecec377a11d8e4
Ray Luna <leon.luna.ray@gmail.com>
2021-03-14 00:23:05 -0800

    upate markup

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems, options);
   });
+
+const aboutNav = document.getElementById('about-nav');
+// const aboutNav = document.getElementById('project-nav');
+// const aboutNav = document.getElementById('contact-nav');
+
+const aboutSection = document.getElementById('about-section');
+// const projectSection = document.getElementById('project-section');
+// const contactSection = document.getElementById('contact-section');
+
+function sectionJump(section) {
+  section.scrollIntoView(true);
+};
+
+// Nav event listeners
+aboutNav.addEventListener('click', sectionJump(aboutSection));
+// projectNav.addEventListener('click', sectionJump(projectSection));
+// contactNav.addEventListener('click', sectionJump(contactSection));
