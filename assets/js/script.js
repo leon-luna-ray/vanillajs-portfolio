3ada5fef7264a2dc6db623c041c4a93d7b974861
Ray Luna <leon.luna.ray@gmail.com>
2023-04-10 08:53:01 -0700

    bug fix on contact section

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,6 @@ const copyrightText = document.createTextNode(year);
 document.querySelector('.copyright').appendChild(copyrightText);
 
 if (profile) {
-  console.log(profile);
-
   if (profile.bio) {
     document.querySelector(
       '#about-section #about-info .about-text'
@@ -30,26 +28,32 @@ if (profile) {
   if (profile.email) {
     const mailto = `mailto:${profile.email}`;
     document.querySelector('#email-link').href = mailto;
-    document.querySelector('#email-link .contact-link').textContent = profile.email;
+    document.querySelector('#email-link .contact-link').textContent =
+      profile.email;
     document.querySelector('#email-link').classList.remove('hide');
   }
   if (profile.github) {
     document.querySelector('#github-link').href = profile.github;
-    document.querySelector('#github-link .contact-link').textContent = profile.github_user || 'Github';
+    document.querySelector('#github-link .contact-link').textContent =
+      profile.github_user || 'Github';
     document.querySelector('#github-link').classList.remove('hide');
+  }
+  if (profile.linkedin) {
+    document.querySelector('#linkedin-link').href = profile.linkedin;
+    document.querySelector('#linkedin-link .contact-link').textContent =
+      profile.linkedin_user || 'Profile';
+    document.querySelector('#linkedin-link').classList.remove('hide');
+  }
+  if (profile.website) {
+    document.querySelector('#website-link').href = profile.website;
+    document.querySelector('#website-link .contact-link').textContent =
+      profile.website_name || 'Link';
+    document.querySelector('#website-link').classList.remove('hide');
   }
   if (profile.image) {
     const profileImageSrc = getImageUrl(profile?.image).size(300, 300).url();
     document.querySelector('#about-section #ray-img img').src = profileImageSrc;
   }
-  // if (!profile.linkedin) {
-
-  //   document.querySelector('#linkedin-link').classList.remove('hide');
-  // }
-  // if(profile.resume) {
-  //   // Todo add resume functionality
-  //   document.querySelector('#resume-link').classList.remove('hide');
-  // }
 }
 
 if (projects.length) {
@@ -57,8 +61,7 @@ if (projects.length) {
     if (index === 0) {
       const card = featuredProjectCard(project);
       featuredProject.insertAdjacentHTML('beforeend', card);
-    }
-    else {
+    } else {
       const card = projectCard(project);
       grid.insertAdjacentHTML('beforeend', card);
     }
