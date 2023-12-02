ebf2aad3137ec01e2df45e00087eb308cc2af808
Ray Luna <leon.luna.ray@gmail.com>
2023-12-02 14:54:10 -0800

    update deploy settings

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import { resolve } from 'path'
 export default defineConfig({
   build: {
     target: 'esnext',
-    // assetsInclude: ['**/*.html'],
-    // rollupOptions: {
-    //   input: {
-    //     main: resolve(__dirname, 'index.html'),
-    //     home: resolve(__dirname, 'src/pages/home.html'),
-    //   },
-    // },
   },
   resolve: {
     alias: {
